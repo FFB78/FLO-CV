0b36ebc37992cf1b0d4cd52d47c533096b7db58b
FFB78 <58028997+FFB78@users.noreply.github.com>
2020-02-16 16:56:48 +0100

    Add files via upload

---
+++ cv-web-brulet-florian/index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+	   <meta charset="utf-8">
+	   <title>Brulet-Florian</title>
+	   <link rel="stylesheet" type="text/css" href="assets/CSS/style.css">
+	   <script type="text/javascript" src="assets/app.js"></script>
+	   <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    </head>
+
+    
+    <body>
+	   <header>
+		<section id="Intro" class="">
+
+		<!----------------Navbar-------------------------->
+			
+			<div class="topnav" id="myTopnav">
+  				<a href="#home" class="active">Home</a>
+ 				<a href="#xp-pro">Cursus</a>
+  				<a href="#portfolios">Expériences</a>
+  				<a href="#contact">Contact</a>
+  				<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+   		    	<i class="fa fa-bars"></i></a>
+			</div>
+
+                    <!------------------Banniere----------------------->
+        <section id="home">
+       		<div class="hero-image bordershadow widthbannier">
+       			<div class="placementcentre">
+       				
+       			<div class="bcwhite taillefixe shadow">
+       				<h2 class="textcenter padding">Brulet Florian</h2>
+       				<h2 class="">Front-Web-Dev</h2><br>
+       				
+       				<div class="dsflex justifyc">
+       					
+       					<div id="text">			
+       					</div>
+       				
+       				<div class="">
+       					 <img class="bdradius" src="assets/IMG/homer-bouddha.jpg">
+       				</div>
+       				</div>
+       			</div>
+       			</div>
+       		</div>
+        </section>
+        
+       <!---Progress Bar de compétence------>
+        <div class="text-cjust mt7">
+        <h2>Skills in progress</h2>
+        </div>
+        <div class="container row">
+
+            <div class="card-bis mt2">
+                <div class="box">
+                    <div class="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70"></circle>
+                            <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div class="number">
+                            <h2>45<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 class="text">Html</h2>
+                </div>
+                
+            </div>
+            <div class="card-bis mt2">
+                <div class="box">
+                    <div class="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70"></circle>
+                            <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div class="number">
+                            <h2>35<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 class="text">CSS</h2>
+                </div>
+                
+            </div>
+            <div class="card-bis mt2">
+                <div class="box">
+                    <div class="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70"></circle>
+                            <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div class="number">
+                            <h2>25<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 class="text">JS</h2>
+                </div>
+                
+            </div>
+        </div>
+
+
+<!--------------cursus scolaire------------------->
+
+      <section id="xp-pro">
+      	<div class="mp">
+        <h2>Mon cursus scolaire</h2>
+        </div>
+        <div class="barre"></div>
+        <div class="flex-row-center mt2">
+          <div class="xp-bloc">
+            <div class="annee">2018/2019</div>
+            <div class="titre-bloc"><i class="fa fa-caret-right" aria-hidden="true"></i>Formation Developpeur Web</div>
+            <div class="sous-titre-bloc">Simplon</div>
+            <div class="nom-formation">Titre certifié</div>
+            <ul>
+              <li>En cours de formation Développeur Web et Web mobile chez Simplon avec certification Méthodes Agiles de gestion et amorçage de projet et OPQUAST-Maitrise de la qualité en projet web</li>
+              <li>
+                En cours de préparation d'un titre certifié 
+              </li>
+            </ul>
+            <div class="langages">HTML5, CSS3, JavaScript</div>
+          </div>
+          <div class="xp-bloc">
+            <div class="annee">2017/2018</div>
+            <div class="titre-bloc"><i class="fa fa-caret-right" aria-hidden="true"></i>E-marketing et E-commerce</div>
+            <div class="sous-titre-bloc">Conservatoire National des Arts et Métiers</div>
+            <div class="nom-formation">Cours du soir</div>
+            <ul>
+              <li>Certificat de compétence E-marketing et E-commerce</li>
+            
+            </ul>
+            <div class="langages"></div>
+          </div>
+        </div>
+        <div class="flex-row-center">
+          <div class="xp-bloc">
+            <div class="annee">2010/2016</div>
+            <div class="titre-bloc"><i class="fa fa-caret-right" aria-hidden="true"></i>Licence Économie et Gestion mention commerce international</div>
+            <div class="sous-titre-bloc">Conservatoire National des Arts et Métiers</div>
+            <div class="nom-formation">Formation en niveau II en Cours du soir</div>
+            <ul>
+              <li>Licence Eco-Gestion</li>
+             
+            </ul>
+            <div class="langages"></div>
+          </div>
+          <div class="xp-bloc">
+            <div class="annee">2005/2006</div>
+            <div class="titre-bloc"><i class="fa fa-caret-right" aria-hidden="true"></i>Apprentissage intensif de la langue anglaise</div>
+            <div class="sous-titre-bloc"></div>
+            <div class="nom-formation"></div>
+            <ul>
+            <br>
+            <br>
+
+
+              <li>Intensive School of English</li>
+              <li>
+                Formation en immersion
+              </li>
+            </ul>
+            <div class="langages"></div>
+          </div>
+        </div>
+       
+      </section>
+
+<!--------Ecpérience professionel--------------->
+
+       <section id="portfolios">
+        <h2>Expériences</h2>
+        <div class="barre"></div>
+        <p>Une séléction de mes précédent poste dans le commerce</p>
+        <div class="flex-row-center l80 ">
+          <img src="./assets/IMG/nespresso-mag.jpg" alt="nespresso"/>
+          <img src="./assets/IMG/sandro1.jpg" alt="sandro"/>
+          <img src="./assets/IMG/lacoste.jpg" alt="lacoste"/>
+          <img src="./assets/IMG/murs.png" alt="4murs"/>
+          <img src="./assets/IMG/wtc.jpg" alt="world tricot"/>
+          <img src="./assets/IMG/pam.jpg" alt="pam"/>
+          <img src="./assets/IMG/katmandou.jpg" alt="katmandou"/>
+          <img src="./assets/IMG/mondial.png" alt="mondialdelauto"/>
+        </div>
+      </section>
+
+<!--------Portofolio 3D Flip-card----------->
+
+<div class="container-cd">
+	
+	<div class="row">
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		
+	</div>
+	<div class="row">
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		
+	</div>
+	<div class="row">
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				
+				</div>
+				<div class="flip-card-back">
+					
+				</div>
+			</div>
+		</div>		
+	</div>
+</div>
+
+<!--------formulaire----------------------------->
+       <section id="home">
+       		<div class="hero-image bordershadow widthbannier">
+       			<div class="placementcentre textcenter">
+       				
+       			<div class="bcwhite taillefixe shadow textcenter">
+       				<h2 class="textcenter padding">Formulaire de contact</h2>
+       				<div class="dsflex justifyc position">
+       					
+       					<div id="contact" class="textcenter width100">
+       						<form id="contact_form" class="textcenter position" method="POST" action="assets/contact.php"> 
+                              <div class="row">
+                                <label class="required" for="name">Your name:</label><br />
+                                <input id="name" class="input" name="name" type="text" size="30" /><br />
+                                <span id="name_validation" class="error_message"></span>
+                              </div>
+                              <div class="row">
+                                <label class="required" for="email">Your email:</label><br />
+                                <input id="email" class="input" name="mail" type="text" size="30" /><br />
+                                <span id="email_validation" class="error_message"></span>
+                              </div>
+                              <div class="row">
+                                <label class="required" for="message">Your message:</label><br />
+                                <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+                                <span id="message_validation" class="error_message"></span>
+                              </div>
+
+                                <button id="submit" type="submit">Envoyer</button>
+                            </form>		
+       					</div>
+       				</div>
+       			</div>
+       			</div>
+       		</div>
+       </section>
+ 
+
+<script type="text/javascript">
+	var i=0,text;
+	text = "Parce que l’ensemble des entreprises passent en mode web, elles sont toutes en quête de sites web performants et originaux. Le développeur web, a donc un rôle prépondérant à jouer autant en ce qui concerne la lisibilité de l’entreprise que sa rentabilité.Fort de plusieurs expériences en tant que commercial je saurais étre à l'ecoute de vos besoins"
+
+	function typing() {
+		if(i<text.length){
+			document.getElementById("text").innerHTML += text.charAt(i);i++;
+			setTimeout(typing,40);
+		}
+	}
+	typing();
+</script>
+
+    </body>
+        </html>
